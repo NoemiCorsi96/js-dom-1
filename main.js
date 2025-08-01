3df3720a9db1f0f04e8ec82b65f25068654fe488
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-08-01 12:44:06 +0200

    first part of exercise

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-//Primo esercizio con DOM 
+//Primo esercizio con DOM
+//vado a selezionare l'immagine, quindi utilizzo il suo tag
+const imgEl = document.querySelector('img')
+console.log(imgEl);
+
+//seleziono il bottone, anche qui posso usare solo il nome del tag
+const buttonEl = document.querySelector('button')
+//per recuperare il percorso dell'img
+console.log(imgEl.src);
+
+
+//Per accendere la lampadina al click sul bottone, devo sare addEventListener
+//\Devo cambiare il percorso della lampadina e mettere quella accesa
+
+buttonEl.addEventListener('click', () => {
+    imgEl.src = './img/yellow_lamp.png'
+    console.log('Lampadina accesa');
+    
+})
